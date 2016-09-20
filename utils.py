b2c8eef3cbd8922e3cf9581e3bc295dc4e872859
mrblackraven777 <cakeman3000@gmail.com>
2016-09-21 00:22:19 +0300

    change utils

--- utils.py
+++ utils.py
@@ -3,9 +3,7 @@ Created on 14 сент. 2016 г.
 
 @author: voskresenskiy
 '''
-import shelve
 from SQLighter import SQLighter
 from config_bot import shelve_name
 def user_came(user_id):
-    with shelve.open(shelve_name) as storage:
-        storage[str(user_id)] = 
+ 
