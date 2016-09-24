4e445151c64971210d98713b3f0dee2cd18d3e72
Unknown <cakeman3000@gmail.com>
2016-09-24 13:12:01 +0300

    SQLIGHT

--- SQLighter.py
+++ SQLighter.py
@@ -5,10 +5,7 @@ class SQLighter:
     def __init__(self, database):
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
-    def select_all(self):
-        """ Получаем все строки """
-        with self.connection:
-            return self.cursor.execute('SELECT * FROM Tabel').fetchall()
+
     def select_single(self, user_id):
         """ Получаем одну строку с нужным нам id """
         with self.connection:
@@ -19,11 +16,7 @@ class SQLighter:
                 return "You have been added"
             else:
                 return "Your id is {}".format(tr[0])
-    def count_rows(self):
-        """ Считаем количество строк """
-        with self.connection:
-            result = self.cursor.execute('SELECT * FROM Tabel').fetchall()
-            return len(result)
+
     def close(self):
         """ Закрываем текущее соединение с БД """
         self.connection.close()
