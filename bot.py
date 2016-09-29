cba7bd26e7698df10752e11a56d4efe59ee8fee5
Unknown <cakeman3000@gmail.com>
2016-09-29 17:49:46 +0300

    heroku test

--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@ import utils
 
 bot = telebot.TeleBot(config_bot.token)
     
+@bot.message_handler(content_type=['text'])       
+bot.send_message(message.chat.id,message.chat.id)
+	
 @bot.message_handler(commands=['came'])   
 def user_came(message):
     bot.send_message(message.chat.id,utils.user_came(message.chat.id))
